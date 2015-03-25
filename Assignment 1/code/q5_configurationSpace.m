5730e758388be5ec84abc0ac75b99ca6eedd9446
jamesFerris <jamesFerris@users.noreply.github.com>
2015-03-25 12:47:49 +1100

    Update q5_configurationSpace.m
    neatened code, comments

--- Assignment 1/code/q5_configurationSpace.m
+++ Assignment 1/code/q5_configurationSpace.m
@@ -1,14 +1,14 @@
-%q5 - configuration space
-close all
-clear
-clc
+%%Q5 - Determine the Configuration Space of the System
+close all;clear;clc
 
-mm = 10^-3;
-DEGREES = pi/180;
-RADIANS = 180/pi;
+%Declare the boundaries of the angles (theta1, theta2, theta3)
+theta1 = [-pi/3, pi/3];
+theta2 = [-2*pi/3, 2*pi/3];
+theta3 = [-pi/2, pi/2];
 
-X = [-pi/3, pi/3];
-Y = [-2*pi/3, 2*pi/3];
-Z = [-pi/2, pi/2];
-
-config_space_3dof(X,Y,Z);
+%Send to a function that will generate the graph
+config_space_3dof(theta1, theta2, theta3);
+title('Configuration Space of the System');
+xlabel('-pi/3 < theta1 < pi/3');
+ylabel('-2pi/3 < theta2 < 2pi/3');
+zlabel('-pi/2 < theta3 < pi/2');
