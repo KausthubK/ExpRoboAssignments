92543427013d0681fef6f80d050279643208e0e3
KausthubK <kausthub.k@live.com>
2015-03-31 20:59:31 +1100

    LSM
    Commented and explained.
    Rectangular (Cartesian Coordinates)
    @TODO: gotta replace this with a polar system

--- Assignment 2/code/LSM.m
+++ Assignment 2/code/LSM.m
@@ -1,5 +1,20 @@
 function [ab, d] = LSM(xpoint,ypoint)
 
+%%%%replace this entire thing with polar coordinate LMS instead)
+
+% %
+% Ax = b (matrix calculations) where:
+% 
+% A = [Mean(xx) Mean(x)]
+%     [Mean(x)  1      ]
+% 
+% x = [Mean(xy)]
+%     [Mean(y) ]
+%
+% b = [gradient]
+%     [intercept]
+
+
 Mx=mean(xpoint);
 My=mean(ypoint);
 
@@ -9,9 +24,11 @@ Mxy=mean(xy);
 xx=times(xpoint,xpoint);
 Mxx=mean(xx)
 
+%assebles our matrices A and Vector x
 A_mat = [Mxx , Mx; Mx, 1];
 x_vec = [Mxy; My];
 
+%finds the determinant of A (if this is zero ignore all the ab values)
 d = det(A_mat);
 
 ab = pinv(A_mat)*x_vec;
