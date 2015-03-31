5952d1bb6d5e3705b747f8039f9744abaf6ac34a
KausthubK <kausthub.k@live.com>
2015-03-31 22:05:45 +1100

    linegen
    test file

--- Assignment 2/code/lineGen.m
+++ Assignment 2/code/lineGen.m
@@ -4,23 +4,34 @@ clc
     
 % data randomiser
 
-% straight line
-xpoint = 4*ones(1, 100);
-ypoint = 1 + (5-1).*rand(1, 100);
+% % vertical straight line (fail case)
+% xpoint = 4*ones(1, 100);
+% ypoint = 1 + (5-1).*rand(1, 100);
+%
 
-%wonky line
+% % % horizontal straight line
+%  %ypoint = 4*ones(1, 100);
+%  xpoint = 1 + (5-1).*rand(1, 100);
+% % %  diagonal line
+%  ypoint = (xpoint.*ones(1, 100));
 
+xpoint = [1 2 3 4 5 6 7 8 9 10 11 12 13 14 15];
+ypoint = [1 1 1 1 1 1 2 3 4 5 6 7  8 9 10];
 
 %plot
 plot(xpoint, ypoint, 'rx');
 hold on;
-[L1, d1] = LSM(xpoint, ypoint);
 
-if(d1 == 0)
-    plot(xpoint, ypoint);
-    hold on;
-else
-    L1Fit = polyval(L1, xpoint);
-    plot(L1Fit, 'g');
-    hold on;
-end
+corners = line_segmentation(xpoint, ypoint, 0.5)
+
+
+%[L1, d1] = LSM(xpoint, ypoint);
+
+% if(d1 == 0)
+%     plot(xpoint, ypoint);
+%     hold on;
+% else
+%     L1Fit = polyval(L1, xpoint);
+%     plot(L1Fit, 'g');
+%     hold on;
+% end
