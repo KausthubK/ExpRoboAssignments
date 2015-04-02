0cc238e7230fe0f88ed0c48877626436ca964daa
sachith1989 <sachith.gunawardhana@gmail.com>
2015-04-02 22:51:24 +1100

    Q6
    Can find the face of the boxes and their centroids to a array. Working
    of referencing that with world cordinates

---
+++ Assignment 2/code/Finding_boxes.m
@@ -0,0 +1,21 @@
+close all
+clear
+clc
+
+I=imread('tt.png');  % read the file
+G=rgb2gray(I); %change it to gray scale
+T=(G<75);       %manualy creating a image with boxes only, using a threshold of 75
+
+T2=bwareaopen(T,3000);      %remove all objects less than 3000 pixels
+se=strel('square',5);   %fill gaps using squares with width of 20
+T3=imclose(T2,se);
+T4=imfill(T3,'holes'); %fill all holes
+
+s=regionprops(T4,'centroid');  %create a stuctured array of s with centorids in all close regions
+centroids=cat(1,s.Centroid); %create a array from s
+imshow(T4)
+hold on
+plot(centroids(:,1),centroids(:,2),'b*') %plot centorids with boxes
+hold off
+
+
