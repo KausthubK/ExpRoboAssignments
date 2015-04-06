cff1f8848f824faa03ee826d3cce1fce68e79a88
sachith1989 <sachith.gunawardhana@gmail.com>
2015-04-06 17:27:50 +1100

    X,Y matches the given fiducials cordinates :dancer:
    I have rotate the image to make centroids of 1st two fiducials vertical.
    now this provides X,Y cordinates which matches the given world
    cordinates

--- Assignment 2/code/Finding_boxes.m
+++ Assignment 2/code/Finding_boxes.m
@@ -13,15 +13,29 @@ T4=imfill(T3,'holes'); %fill all holes
 
 s=regionprops(T4,'centroid');  %create a stuctured array of s with centorids in all close regions
 centroids=cat(1,s.Centroid); %create a array from s
-imshow(T4)
+%imshow(T4)
+%hold on
+%plot(centroids(:,1),centroids(:,2),'b*') %plot centorids with boxes
+%hold off
+
+%Rotating the image to make reference points vertical
+x1=centroids(:,1);
+y1=centroids(:,2);
+
+theta=atand((x1(1)-x1(2))/(y1(1)-y1(2))); %theta degrees between 1st two centroids and y axis
+T5=imrotate(T4,-theta);   %rotate image theta degrees
+
+figure
+s2=regionprops(T5,'centroid');  %create a stuctured array of s with centorids in all close regions
+centroids2=cat(1,s2.Centroid); %create a array from s
+imshow(T5)
 hold on
-plot(centroids(:,1),centroids(:,2),'b*') %plot centorids with boxes
+plot(centroids2(:,1),centroids2(:,2),'b*') %plot centorids with boxes
 hold off
 
 %matching x,y in the world cordinates
-x=centroids(:,1);
-y=centroids(:,2);
+x2=centroids2(:,1);
+y2=centroids2(:,2);
 
-Y=x*(523-220)/(724.1845-291.9483)+15.3426; %Y cordinate in the world cordinate
-X=(y-480)*(540/(876.9374-85.0221)); %X cordinate in the world cordinate
-
+Y=x2*(523-220)/(734.8936-302.6777)+7.8113; %Y cordinate in the world cordinate
+X=(y2-480)*(540/(881.7552-89.8304))-3.95; %X cordinate in the world cordinate
