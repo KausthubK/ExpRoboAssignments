72e6f69e70ea99152ccbdeb35c6ad41cd5d5e2aa
sachith1989 <sachith.gunawardhana@gmail.com>
2015-04-15 23:41:23 +1000

    New code for Finding boxes(Finding_boxes_new)
    Guys I have done some modifications to my code. Following changes have
    been done
    1. remove shadows before find top face: Now this finds the box better
    than prvs. And will work on different shadows
    2.Remove 3 fiducial boxes from the centroid list to use them as
    references
    3.Has used a better referencing method to match with world cordinate.
    (Now no need to rotate the image which is so stupid anyway)
    
    One step left.

--- Assignment 2/code/Q6/Finding_boxes.m
+++ Assignment 2/code/Q6/Finding_boxes.m
@@ -3,10 +3,18 @@ clear
 clc
 
 I=imread('tt.png');  % read the file
-G=rgb2gray(I); %change it to gray scale
-T=(G<75);       %manualy creating a image with boxes only, using a threshold of 75
 
-T2=bwareaopen(T,3000);      %remove all objects less than 3000 pixels
+%remove shadow
+I2=rgb2hsv(I); %convert rgb to high saturation version
+C1=I2(:,:,2); %get the bw from G
+C2=C1<0.4; % find shadow using manual threshold
+C3=C2<1; % transverse black n white
+Tgray=I2(:,:,3); % Bw of Image
+X=Tgray+C3; %remove shadow
+
+X2=X<0.33; 
+
+T2=bwareaopen(X2,3000);      %remove all objects less than 3000 pixels
 se=strel('square',5);   %fill gaps using squares with width of 20
 T3=imclose(T2,se);
 T4=imfill(T3,'holes'); %fill all holes
@@ -21,6 +29,43 @@ centroids=cat(1,s.Centroid); %create a array from s
 %Rotating the image to make reference points vertical
 x1=centroids(:,1);
 y1=centroids(:,2);
+
+%find fiducial box_1 (-270,220)
+for i=1:length(x1)
+    if ((x1(i)< 297 & x1(i)>287)& (y1(i)<88 & y1(i)>78 ))
+        B1=[x1(i),y1(i)];
+    end
+end
+
+x1(B1(1,1)==x1)=[];
+y1(B1(1,2)==y1)=[];
+
+%find fiducial box_2 (270,220)
+for i=1:length(x1)
+    if ((x1(i)< 305 & x1(i)>295)& (y1(i)<881 & y1(i)>871 ))
+        B2=[x1(i),y1(i)];
+    end
+end
+
+x1(B2(1,1)==x1)=[];
+y1(B2(1,2)==y1)=[];
+
+%find fiducial box_3 (-270,523)
+for i=1:length(x1)
+    if ((x1(i)< 731 & x1(i)>721)& (y1(i)<81 & y1(i)>71 ))
+        B3=[x1(i),y1(i)];
+    end
+end
+
+x1(B3(1,1)==x1)=[];
+y1(B3(1,2)==y1)=[];
+
+
+TF=isempty(x1);
+if TF==1
+    warning('No objects detected')
+end
+
 
 theta=atand((x1(1)-x1(2))/(y1(1)-y1(2))); %theta degrees between 1st two centroids and y axis
 T5=imrotate(T4,-theta);   %rotate image theta degrees
---
+++ Assignment 2/code/Q6/Finding_boxes_new.m
@@ -0,0 +1,88 @@
+close all
+clear
+clc
+
+I=imread('tt2.png');
+
+%remove shadow
+I2=rgb2hsv(I); %convert rgb to high saturation version
+T=I2(:,:,2); %get the bw from G
+T1=T<0.3; % find shadow using manual threshold
+T2=T1<1; % transverse black n white
+Tgray=I2(:,:,3); % Bw of Image
+X=Tgray+T2; %remove shadow
+
+X2=X<0.33; %find box
+
+C1=bwareaopen(X2,3000);
+se=strel('square',5);
+C3=imclose(C1,se);
+C4=imfill(C3,'holes');
+
+%imshow(C4)
+%subplot(2,2,1), imshow(I);
+%subplot(2,2,2), imshow(C1);
+
+s=regionprops(C4,'centroid');
+centroids=cat(1,s.Centroid);
+% imshow(C4)
+% hold on
+% plot(centroids(:,1),centroids(:,2),'b*') %plot centorids with boxes
+% hold off
+
+x1=centroids(:,1);
+y1=centroids(:,2);
+
+
+%find fiducial box_1 (-270,220)
+% B1(1,1)=x1(x1(:,1)<297 & x1(:,1)>288);
+% B1(1,2)=y1(y1(:,1)<88 & y1(:,1)>78);
+% x1(B1(1,1)==x1)=[];
+% y1(B1(1,2)==y1)=[];
+
+for i=1:length(x1)
+    if ((x1(i)< 297 & x1(i)>287)& (y1(i)<88 & y1(i)>78 ))
+        B1=[x1(i),y1(i)];
+    end
+end
+
+x1(B1(1,1)==x1)=[];
+y1(B1(1,2)==y1)=[];
+
+%find fiducial box_2 (270,220)
+for i=1:length(x1)
+    if ((x1(i)< 305 & x1(i)>295)& (y1(i)<881 & y1(i)>871 ))
+        B2=[x1(i),y1(i)];
+    end
+end
+
+x1(B2(1,1)==x1)=[];
+y1(B2(1,2)==y1)=[];
+
+%find fiducial box_3 (-270,523)
+for i=1:length(x1)
+    if ((x1(i)< 731 & x1(i)>721)& (y1(i)<81 & y1(i)>71 ))
+        B3=[x1(i),y1(i)];
+    end
+end
+
+x1(B3(1,1)==x1)=[];
+y1(B3(1,2)==y1)=[];
+
+
+TF=isempty(x1);
+if TF==1
+    warning('No objects detected other than fiducial boxes')
+end
+
+Yr=abs(p_dist_q6(B1,B2,x1,y1)); %distance from line going along centroids of box1 & box 2 (reference)
+Xr=p_dist_q6(B1,B3,x1,y1);  %distance from line going along centroids of box1 & box 3 (reference)
+
+%find the reference point according to the world cordinates
+    %X 792.8691 = 540 real , %Y 433.3300 = 303 real                                    
+Yreal=((Yr*303)/433.3300)+220;    %REAL Y CORDINATE 
+Xreal=((Xr*540)/792.8691)-270;    %REAL x CORDINATE
+
+% subplot(2,2,1), imshow(I); title 'Original' 
+% subplot(2,2,2), imshow(C4); title 'with centroids', hold on, plot(centroids(:,1),centroids(:,2),'b*'); hold off 
+% subplot(2,2,3), imshow(T1); title 'shadow'
