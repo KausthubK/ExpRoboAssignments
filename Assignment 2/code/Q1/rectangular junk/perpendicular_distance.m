778c3410a5e551ffcd3192291398641dac02bd25
sachith1989 <sachith.gunawardhana@gmail.com>
2015-04-15 23:47:16 +1000

    Mistake in perpendicular distance
    There was a mistake in this function. Now it is corrected
    
    Kaustub: Did you use this for Q1. If so check it again now.

--- Assignment 2/code/Q1/rectangular junk/perpendicular_distance.m
+++ Assignment 2/code/Q1/rectangular junk/perpendicular_distance.m
@@ -2,9 +2,9 @@ function [pDist] = perpendicular_distance(xpoint,ypoint,a,b)
  
 LP1=[-b/a,0];
 LP2=[0,b];
-D=sqrt((LP2(2)-LP1(1))^2+(LP2(2)-LP1(2))^2);
+D=sqrt((LP2(1)-LP1(1))^2+(LP2(2)-LP1(2))^2);
 
-r=(xpoint*(LP1(2)-LP2(2))+ypoint*(LP2(1)-LP1(1))+LP2(2)*LP1(1)-LP1(1)*LP2(1));
+r=(xpoint*(LP1(2)-LP2(2))+ypoint*(LP2(1)-LP1(1))+LP2(2)*LP1(1)-LP1(2)*LP2(1));
 
 pDist=r/D;
 
