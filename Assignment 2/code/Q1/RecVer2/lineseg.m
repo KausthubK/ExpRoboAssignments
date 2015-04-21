bc014ccf1c2d392e2d1f2f3b23abf2b08285f193
KausthubK <kausthub.k@live.com>
2015-04-21 22:10:31 +1000

    lineseg done needs testing
    about to test Q1 c.
    a & b work

--- Assignment 2/code/Q1/RecVer2/lineseg.m
+++ Assignment 2/code/Q1/RecVer2/lineseg.m
@@ -4,12 +4,12 @@ function corners = lineseg(xPoints, yPoints, linThres)
 	cornerInds = [1, iEnd];
 	newInds = lineseg_recurs(iStart, iEnd, xPoints, yPoints, linThres);
 	cornerInds = [cornerInds, newInds];
+	cornerInds = sort(cornerInds);
 end
 
 function tempToConcat = lineseg_recurs(iStart, iEnd, xPoints, yPoints, linThres)
 	maxPDist = 0;
 	maxPDInd = 0;
-
 	tempLine = leastsqmin(xPoints, yPoints, iStart, iEnd)
 
 	for n = iStart:iEnd
@@ -22,8 +22,11 @@ function tempToConcat = lineseg_recurs(iStart, iEnd, xPoints, yPoints, linThres)
 
 	if maxPDist <= linThres
 		disp 'linear... hell yeah'
+		tempToConcat = [iEnd];
 	else
-		
+		disp 'not linear'
+		left_ttc = lineseg_recurs(iStart, maxPDInd, xPoints, yPoints, linThres);
+		right_ttc = lineseg_recurs(maxPDInd, iEnd, xPoints, yPoints, linThres);
+		tempToConcat = [left_ttc, right_ttc];
 	end
-
 end
