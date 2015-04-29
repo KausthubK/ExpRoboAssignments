7701d10e5a2c20e12bd1f5d6e5f8e57e5df02af8
jamesFerris <james.ferris0@gmail.com>
2015-04-29 13:28:45 +1000

    data fusion
    get input for pred stage

--- A3/q1/dataFusion.m
+++ A3/q1/dataFusion.m
@@ -4,3 +4,13 @@ clc
 
 
 
+velocityObs = load('velocityObs.txt');
+
+
+time = (velocityObs(:,1)*10^6) + velocityObs(:,2); %get in microseconds
+velocity = velocityObs(:,3);
+turnRate = velocityObs(:,4);
+deltaT = zeros(length(time),1);
+for i = 2:length(time)
+    deltaT(i) = time(i) - deltaT(i-1);           
+end
