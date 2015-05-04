77ec5c51e7c11abd9cb77cbb374020b65d1047bb
KausthubK <kausthub.k@live.com>
2015-05-04 18:56:54 +1000

    Compass Halfway
    not sure how to correct wraparound error stuff

---
+++ A3/q1/dataFusion_v3.m
@@ -0,0 +1,189 @@
+clear
+close all
+clc
+
+DEGREES = 180/pi;
+RADIANS = pi/180;
+
+% % Prediction Stage
+
+%Load observational data
+velocityObs = load('velocityObs.txt');
+positionObs = load('positionObs.txt');
+compassObs = load('compassObs.txt');
+laserObs = load('laserObs.txt');
+
+%Get velocity data
+time1 = velocityObs(:,1) + (velocityObs(:,2)*10^-6) - 1115116000;%get in microseconds
+velocity = velocityObs(:,3);
+turnRate = velocityObs(:,4);
+
+velObs = [time1 velocity turnRate];
+
+%Get GPS position data
+time2 = positionObs(:,1) + (positionObs(:,2)*10^-6) - 1115116000;
+xPos = positionObs(:,3);
+yPos = positionObs(:,4);
+
+posObs = [time2 xPos yPos];
+
+%Get GPS compass data
+time3 = compassObs(:,1) + (compassObs(:,2)*10^-6) - 1115116000;
+heading = compassObs(:,3);
+
+compObs = [time3 heading];
+
+% % %get laser data
+time4 = laserObs(:,1) + (laserObs(:,2)*10^-6) - 1115116000;
+% % i = 1;
+% % j = 4;
+% % sizeLas = size(laserObs);
+% % lasObs = zeros(sizeLas(1, 2));
+% % while(i <= sizeLas(1))
+% %     lasObs(i, 1) = [time4(i)];
+% %     while(j<=sizeLas(2))
+% %         lasObs(i, j) = laserObs(i, j);
+% %         lasObs(i, j-1) = laserObs(i, j-1);
+% %         
+% %         j = j + 2;
+% %     end
+% %     
+% %     i = i + 1;
+% % end
+% % size(time4)
+
+
+
+%%postInput parsing
+
+alphaP = 0.5;
+alphaTH = 0.5;
+
+lastTime = 0;
+deltaT = 0;
+
+latestVel = 0;
+latestTurnRate = 0;
+
+ourX = 0;
+ourY = 0;
+ourHeading = 0;
+
+indLengths = [length(time1), length(time2), length(time3), length(time4)];
+maxIters = max(indLengths);
+
+% output = [lastTime, ourX, ourY, ourHeading];
+output = zeros(maxIters, 4);
+
+% deadreckonedpts = zeros(maxIters, 3);
+
+%iters [velInd, posInd, compInd, lasInd];
+iters = [2, 2, 2, 2];
+runFlags = [0, 0, 0, 0];
+loopFlag = 1;
+loopCount = 2
+%%loop starts
+while(loopFlag == 1)
+    loopCount = loopCount + 1
+    time = [time1(iters(1)), time2(iters(2)), time3(iters(3)), time4(iters(4)),];
+    nextT = min(time);
+    
+    
+    for i = 1:4
+        if time(i) == nextT
+            runFlags(i) = 1;
+        else
+            runFlags(i) = 0;
+        end
+    end
+    
+    %if velocityobs
+    if(runFlags(1) == 1)
+        deltaT = time1(iters(1)) - lastTime;              
+        latestVel = velObs(iters(1),2);%
+        latestTurnRate = velObs(iters(1),3);%
+        pr = predictionStage(ourX, ourY, ourHeading, deltaT, latestTurnRate, latestVel);
+        ourX = pr(1);
+        ourY = pr(2);
+        ourHeading = pr(3);
+        
+%         deadreckonedpts(1) = ourX;
+%         deadreckonedpts(2) = ourY;
+%         deadreckonedpts(3) = ourHeading;
+        lastTime = time1(iters(1));%
+        runFlags(1) = 0;
+        if iters(1) == length(time1)
+            time1(iters(1)) = 1.496*10^8;
+        else
+            iters(1) = iters(1) + 1;
+        end
+    end
+    
+% % if GPS
+     if(runFlags(2) == 1)
+        deltaT = time2(iters(2)) - lastTime;
+        pr = predictionStage(ourX, ourY, ourHeading, deltaT, latestTurnRate, latestVel);
+        gUpd = updateStageGPS(pr(1), pr(2), posObs(iters(2),2), posObs(iters(2),3), alphaP); %xvobs and yvobs need to come from the file
+        ourX = gUpd(1);
+        ourY = gUpd(2);
+        ourHeading = pr(3);
+        lastTime = posObs(iters(2),1);%
+        runFlags(2) = 0;
+        if iters(2) == length(time2)
+            time2(iters(2)) = 1.496*10^8;
+        else
+            iters(2) = iters(2) + 1;
+        end
+     end
+%     
+% % if compass    
+     if(runFlags(3) == 1)
+        deltaT = time3(iters(3)) - lastTime;
+        pr = predictionStage(ourX, ourY, ourHeading, deltaT, latestTurnRate, latestVel);
+        cUpd = updateStageCompass(pr(3), compObs(iters(3),2), alphaTH);
+        ourX = pr(1);
+        ourY = pr(2);
+        ourHeading = cUpd;
+        lastTime = compObs(iters(3),1);
+        runFlags(3) = 0;
+        if iters(3) == length(time3)
+            time3(iters(3)) = 1.496*10^8;
+        else
+            iters(3) = iters(3) + 1;
+        end
+    end
+%     
+% % if laser data
+    if(runFlags(4) == 1)
+% %       fill this in
+        runFlags(4) = 0;
+        time4(iters(4)) = 1.496*10^8;   %remove this line
+    end
+
+%check loop
+    if(time1(iters(1)) == 1.496*10^8)
+        if(time2(iters(2)) == 1.496*10^8)
+            if(time3(iters(3)) == 1.496*10^8)
+                if(time4(iters(4)) == 1.496*10^8)
+                     loopFlag = 0;
+                end
+            end
+        end
+    end
+    
+%plot stuff
+hold on
+title('Robot Path');
+xlabel('x-axis');
+ylabel('y-axis');
+% legend('')
+% drawnow
+plot(deadreckonedpts(1), deadreckonedpts(2), 'b.');
+plot(ourX, ourY, 'rx');
+output(loopCount, 1) = lastTime;
+output(loopCount, 2) = ourX;
+output(loopCount, 3) = ourY;
+output(loopCount, 4) = ourHeading;
+end
+
+output(:,1) = output(:,1); %+ 1115116000;
--- A3/q1/updateStageCompass.m
+++ A3/q1/updateStageCompass.m
@@ -1,5 +1,6 @@
 function THvUpd = updateStageCompass(THvPred, THvObs, alphaTH)
 
-THvUpd = (1 - alphaTH)*THvPred + alphaTH*THvObs;
+%THvUpd = (1 - alphaTH)*THvPred + alphaTH*THvObs;
+THvUpd = THvPred - alphaTH*(THvObs - THvPred);
 
 end
