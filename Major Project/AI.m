7c4f93df7bb9cad4d58c774e6b48f03157f4c7d3
KausthubK <kausthub.k@live.com>
2015-05-20 12:09:03 +1000

    AI
    main skeleton started

---
+++ Major Project/AI.m
@@ -0,0 +1,5 @@
+% this is the overarching game playing software
+% the pseudo code is as follows
+% % start
+% % % check how many cards are face down
+% % % % if less than two are face down -> go to end game
