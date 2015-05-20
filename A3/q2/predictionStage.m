173f6c7820acb52330e2ecc83681011ca41e1465
sachith1989 <sachith.gunawardhana@gmail.com>
2015-05-20 17:14:52 +1000

    Clean folder
    unwanted files I created in A3

--- A3/q2/predictionStage.m
+++
@@ -1,7 +0,0 @@
-function pr = predictionStage( XvPrev, YvPrev, THvPrev, deltaT, currTurnRate, currVel )
-%Prediction Function given previous values
-    XvPred = XvPrev + deltaT*currVel*cos(THvPrev);
-    YvPred = YvPrev + deltaT*currVel*sin(THvPrev);
-    THvPred = THvPrev + deltaT*currTurnRate;
-    pr = [XvPred, YvPred, THvPred];
-end
