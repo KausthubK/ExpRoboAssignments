2082750fa02542a394ed38197960cab28a60296c
sachith1989 <sachith.gunawardhana@gmail.com>
2015-05-21 12:34:14 +1000

    numCards/numFUP/numFDWN/isolated Faceup

--- Major Project/code/Find_Cards.m
+++ Major Project/code/Find_Cards.m
@@ -3,9 +3,18 @@ clear
 clc
 
 %read the image
-I=imread('test_image11.png');
+I=imread('test_image12.png');
 BW=double(I)/255; %convert to double
 
+%FIND FACEDOWN CARDS
+T1=BW<0.1;
+T2=imclearborder(T1,4);
+T3=imfill(T2,'holes');
+FDWN=bwareaopen(T3,1000);
+CC1=bwconncomp(FDWN);
+numFDWN=CC1.NumObjects;
+
+%FIND FACEUP CARDS
 I2=BW>0.91;  %identify faceup card
 I3=imfill(I2,'holes'); %fill all holes but the shape was gone
 
@@ -31,12 +40,20 @@ cards(mask)=0;
 figure, imshow(cards)
 % imwrite(cards,'t3.png');
 
-CC=bwconncomp(cards); %find the connected blobs
-s2=regionprops(CC,'BoundingBox'); %identify the card boundary
+CC2=bwconncomp(cards); %find the connected blobs
+numFUP=CC2.NumObjects; %number of Faceup cards
+s2=regionprops(CC2,'BoundingBox'); %identify the card boundary
 face_up=imcrop(BW,s2.BoundingBox);
 figure,imshow(face_up);
+
+numCards=numFUP+numFDWN; %number of cards
 
 %find Features
 corners = detectFASTFeatures(cards);
 figure, imshow(BW); hold on
 plot(corners)
+
+
+numCards
+numFUP
+numFDWN
