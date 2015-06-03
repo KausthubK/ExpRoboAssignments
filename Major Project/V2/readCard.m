6b71ba7ad8f4c7ae7ed0aa4b83aaab27349f8cb1
sachith1989 <sachith.gunawardhana@gmail.com>
2015-06-03 23:08:55 +1000

    Test images Added. Finding shape solved
    Worked on version 2 though.

--- Major Project/V2/readCard.m
+++ Major Project/V2/readCard.m
@@ -55,104 +55,112 @@ se=strel('square',9);
 IE=imerode(I3,se);
 I4=imclearborder(IE,4);
 Iout=bwareaopen(I4,1000);
+U=bwconncomp(Iout);
+N=U.NumObjects;
 
-Bwoutline=bwperim(Iout); %create a outline
-border=Icheck;
-border(Bwoutline)=0;
-% figure, imshow(border);
+if N>0;
+    Bwoutline=bwperim(Iout); %create a outline
+    border=Icheck;
+    border(Bwoutline)=0;
+    % figure, imshow(border);
 
-%ISOLATE FACEUP
-cards=Icheck;
-mask=Iout<0.5; %create a mask to isolate the face up
-cards(mask)=0;
-CC2=bwconncomp(cards); %find the connected blobs
+    %ISOLATE FACEUP
+    cards=Icheck;
+    mask=Iout<0.5; %create a mask to isolate the face up
+    cards(mask)=0;
+    CC2=bwconncomp(cards); %find the connected blobs
 
-s2=regionprops(CC2,'BoundingBox'); %identify the card boundary
+    s2=regionprops(CC2,'BoundingBox'); %identify the card boundary
 
 
-%EXTRACT ISOLATED FACEUP
-face_up(1).image=imcrop(Icheck,s2(1).BoundingBox);
+    %EXTRACT ISOLATED FACEUP
+    face_up(1).image=imcrop(Icheck,s2(1).BoundingBox);
 
-%DETECT FEATURES
-E=edge(face_up(1).image,'canny');
-T10=imfill(E,'holes');
-T11=bwareaopen(T10,200);
+    %DETECT FEATURES
+    E=edge(face_up(1).image,'canny');
+    T10=imfill(E,'holes');
+    T11=bwareaopen(T10,200);
 
-CC3=bwconncomp(T11);
-s3=regionprops(CC3,'Perimeter');
+    CC3=bwconncomp(T11);
+    s3=regionprops(CC3,'Perimeter');
 
-for i=1:length(s3)
-    face_up(1).perim(i)=s3(i).Perimeter;
-end
+    for i=1:length(s3)
+        face_up(1).perim(i)=s3(i).Perimeter;
+    end
 
-face_up(1).MeanPerim=mean(face_up(1).perim);
+    face_up(1).MeanPerim=mean(face_up(1).perim);
 
-%DEFINING CARD
-%SHAPE
-face_up(1).shape = 0;
-if face_up(1).MeanPerim<162 && face_up(1).MeanPerim>155;
-    face_up(1).shape=1; %1 rectangle in set card
-elseif face_up(1).MeanPerim<90 && face_up(1).MeanPerim>85;
-    face_up(1).shape=2; %1 trangle in set card
-elseif face_up(1).MeanPerim<137 && face_up(1).MeanPerim>132;
-    face_up(1).shape=3; %1 elipse in set card
-end
+    %DEFINING CARD
+    %SHAPE
+    face_up(1).shape = 0;
+    if face_up(1).MeanPerim<165 && face_up(1).MeanPerim>145;
+        face_up(1).shape=1; %1 rectangle in set card
+    elseif face_up(1).MeanPerim<124 && face_up(1).MeanPerim>116;
+        face_up(1).shape=2; %1 trangle in set card
+    elseif face_up(1).MeanPerim<137 && face_up(1).MeanPerim>125;
+        face_up(1).shape=3; %1 elipse in set card
+    end
 
-%SHAPE COUNT
-face_up(1).count=CC3.NumObjects;
+    %SHAPE COUNT
+    face_up(1).count=CC3.NumObjects;
 
-%FILLER
-TT=face_up(1).image;
-st2=regionprops(T11,TT,'MeanIntensity');
+    %FILLER
+    TT=face_up(1).image;
+    st2=regionprops(T11,TT,'MeanIntensity');
 
-for i=1:length(st2)
-    face_up(1).Intensity(i)=st2(i).MeanIntensity;
-end
+    for i=1:length(st2)
+        face_up(1).Intensity(i)=st2(i).MeanIntensity;
+    end
 
-face_up(1).MeanIntensity=mean(face_up(1).Intensity);
+    face_up(1).MeanIntensity=mean(face_up(1).Intensity);
 
-if face_up(1).MeanIntensity>0.6
-    face_up(1).filler=1; %not shaded
-elseif face_up(1).MeanIntensity<0.6 && face_up(1).MeanIntensity>0.5
-    face_up(1).filler=2; %shaded
-elseif face_up(1).MeanIntensity<0.5
-    face_up(1).filler=3; %block
-end
+    if face_up(1).MeanIntensity>0.6
+        face_up(1).filler=1; %not shaded
+    elseif face_up(1).MeanIntensity<0.6 && face_up(1).MeanIntensity>0.5
+        face_up(1).filler=2; %shaded
+    elseif face_up(1).MeanIntensity<0.5
+        face_up(1).filler=3; %block
+    end
 
-%OUTPUT VALUES
-shape = face_up(1).shape;
-filler = face_up(1).filler;
-colour = face_up(1).colour;
-count = face_up(1).count;
+    %OUTPUT VALUES
+    shape = face_up(1).shape;
+    filler = face_up(1).filler;
+    colour = face_up(1).colour;
+    count = face_up(1).count;
 
-%DISPLAY
-if face_up(1).shape==1
-    disp 'RECTANGLE'
-    viewed = 1;
-elseif face_up(1).shape==2
-    disp 'TRANGLE'
-    viewed = 1;
-elseif face_up(1).shape==3
-    disp 'ELIPSE'
-    viewed = 1;
-elseif face_up(1).shape==0
-	disp 'UNABLE TO DETERMINE SHAPE'
-	viewed = 0;
-end
+    %DISPLAY
+    if face_up(1).shape==1
+        disp 'RECTANGLE'
+        viewed = 1;
+    elseif face_up(1).shape==2
+        disp 'TRANGLE'
+        viewed = 1;
+    elseif face_up(1).shape==3
+        disp 'ELIPSE'
+        viewed = 1;
+    elseif face_up(1).shape==0
+        disp 'UNABLE TO DETERMINE SHAPE'
+        viewed = 0;
+    end
 
-if face_up(1).filler == 3
-    disp 'BLOCK'
-elseif face_up(1).filler==2
-    disp 'SHADED'
-elseif face_up(1).filler==1
-    disp 'NOT SHADED'
+    if face_up(1).filler == 3
+        disp 'BLOCK'
+    elseif face_up(1).filler==2
+        disp 'SHADED'
+    elseif face_up(1).filler==1
+        disp 'NOT SHADED'
+    else
+        disp 'UNABLE TO DETERMINE FILLER'
+    end
+
+    fprintf('Shape count = %d\n\n',count);
+
+    % imshow(cards);
 else
-    disp 'UNABLE TO DETERMINE FILLER'
+    viewed=-1;
+end    
+    
 end
 
-fprintf('Shape count = %d\n\n',count);
 
-% imshow(cards);
-
-end
-
+    
